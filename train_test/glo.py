82de1bc1ac30d81c1132f3a33f8a1eed6141ae61
naked grandma <105282871+baipiaoguaiisme@users.noreply.github.com>
2025-06-04 22:11:27 +0800

    Delete train_test directory

--- train_test/glo.py
+++
@@ -1,9 +0,0 @@
-def _init():
-    global _global_dict
-    _global_dict = {}
-
-def set_value(key, value):
-    _global_dict[key] = value
-
-def get_value(key, defValue=None):
-    return _global_dict[key]
